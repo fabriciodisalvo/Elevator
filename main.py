fb39c6054017e6a1f27ec38c40c0c29320a70b23
fabriciodisalvo <fabriciodisalvo@gmail.com>
2020-10-03 21:29:22 -0300

    Add nombres.csv, a NAME resource list to create people that boards the Elevator.

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from elevator import *
 la_pampa = Building("La Pampa", 12)
 la_pampa_01 = Elevator("Ascensor 01", la_pampa, 6)
 
-la_pampa.add_person(0, "Fabri", 12)
-la_pampa.add_person(0, "Rusa", 12)
+names_file = open("nombres.csv", "r")
+names = names_file.read().split(",")
+names_file.close()
+
+la_pampa.add_person(0, names[0], 12)
+la_pampa.add_person(11, names[1], 0)
 la_pampa_01.run_elevator()
