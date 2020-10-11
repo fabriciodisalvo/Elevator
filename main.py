3bc935ddfb639e1d5aedb0b3fd1c9c448c1cc74e
fabriciodisalvo <fabriciodisalvo@gmail.com>
2020-10-11 11:31:58 -0300

    Add randomizer for passenger names.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from elevator import *
+from random import randint
 
 la_pampa = Building("La Pampa", 12)
 la_pampa_01 = Elevator("Ascensor 01", la_pampa, 6)
@@ -7,7 +8,6 @@ names_file = open("nombres.csv", "r")
 names = names_file.read().split(",")
 names_file.close()
 
-la_pampa.add_person(0, names[0], 12)
-la_pampa.add_person(11, names[1], 0)
-la_pampa.print_waiting_list()
-la_pampa_01.run_elevator()
+for i in range(1):
+    la_pampa.add_person(0, names[randint(0, len(names))], randint(1, 12))
+    la_pampa.add_person(randint(1, 12), names[randint(0, len(names))], 0)
