9fb08ec89e84ee00b704d653a1c4b5ee31301769
fabriciodisalvo <fabriciodisalvo@gmail.com>
2020-10-11 22:43:01 -0300

    Changed main.py to use generic names.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from elevator import *
 from random import randint
 
-la_pampa = Building("La Pampa", 12)
-la_pampa_01 = Elevator("Ascensor 01", la_pampa, 6)
+my_building = Building("My Office Building", 12)
+my_building_01 = Elevator("01", my_building, 6)
 
 names_file = open("nombres.csv", "r")
 names = names_file.read().split(",")
@@ -12,4 +12,4 @@ passenger_list = []
 for i in range(10):
     passenger_list.append([0, names[randint(0, len(names)-1)], randint(1, 12)])
     passenger_list.append([randint(1, 12), names[randint(0, len(names)-1)], 0])
-la_pampa.add_passengers(passenger_list)
+my_building.add_passengers(passenger_list)
