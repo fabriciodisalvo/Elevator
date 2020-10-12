706efe7f48790703db8bba8f3a2b25412a3f2930
fabriciodisalvo <fabriciodisalvo@gmail.com>
2020-10-11 22:12:06 -0300

    Modify run_elevator(), Add sort_cue()
    Changed function run_elevator() to use an updated sorted cue,
    modified by function sort_cue(), which sorts the cue with all
    remaining upper floors in ascending order, plus all the remaining
    lower floor in descending order.

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ names = names_file.read().split(",")
 names_file.close()
 
 passenger_list = []
-for i in range(1):
-    passenger_list.append([randint(1, 12), names[randint(0, len(names))], 0])
-    passenger_list.append([0, names[randint(0, len(names))], randint(1, 12)])
-
+for i in range(10):
+    passenger_list.append([0, names[randint(0, len(names)-1)], randint(1, 12)])
+    passenger_list.append([randint(1, 12), names[randint(0, len(names)-1)], 0])
 la_pampa.add_passengers(passenger_list)
